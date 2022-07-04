d83fe0006915c21d79763be5db94a8a4e8af9866
Abraham <cartes5ironman@gmail.com>
2022-07-04 09:37:31 +0200

    Creacion de Portal

---
+++ client/src/components/Utilities/UserMainWelcome/UsermainWelcome.jsx
@@ -0,0 +1,30 @@
+import classes from "./UserMainWelcome.module.css";
+import Button from "../Button/Button";
+import { useSelector, useDispatch } from "react-redux";
+import { isLoggedInActions } from "../../../features/isLoggedIn";
+import { currentClassActions } from "../../../features/currentClass";
+import { currentUserActions } from "../../../features/currentUser";
+import { wrapperActions } from "../../../features/wrapper";
+const UserMainWelcome = () => {
+  const dispatch = useDispatch();
+  const user = useSelector((state) => state.CurrentUser.user);
+  const dark = useSelector((state) => state.DarkMode.isDarkMode);
+
+  const logOutHandler = () => {
+    dispatch(currentClassActions.clearClass());
+    dispatch(currentUserActions.clearCurrentUser());
+    dispatch(isLoggedInActions.setIsLoggedIn());
+    dispatch(wrapperActions.setInitial());
+  };
+
+  return (
+    <article className={classes.article}>
+      <h3 className={dark ? `${classes.darkH2}` : `${classes.lightH2}`}>
+        BIENVENIDO {user.fName}
+      </h3>
+      <Button clickMe={logOutHandler} innerTxt={"Log Out"} />
+    </article>
+  );
+};
+
+export default UserMainWelcome;
